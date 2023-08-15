ad3d1b97cade8baf556e45f7ff77c072578fc88e
Евгений Галимсаров <address@email.com>
2023-08-15 11:02:15 +0500

    HTTP GET with Spring RestTemplate

---
+++ src/main/java/guru/springframework/msscbreweryclient/web/model/CustomerDto.java
@@ -0,0 +1,17 @@
+package guru.springframework.msscbreweryclient.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
+public class CustomerDto {
+    private UUID id;
+    private String name;
+}
